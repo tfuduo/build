32c9dbb08ac70dc680aa334f73372495c20d579a
fuduo <329794799@qq.com>
2020-05-29 12:28:23 +0800

    upload

---
+++ precache-manifest.028b10591e2c331ec68c22b2e16aecd6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1efa53f9255f113d0df4f01b7f98f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb7e435c01137b642edc",
+    "url": "/static/css/main.cccdf7f5.chunk.css"
+  },
+  {
+    "revision": "6c1c9430fb02d35f6cf1",
+    "url": "/static/js/2.e192f2b9.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/static/js/2.e192f2b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb7e435c01137b642edc",
+    "url": "/static/js/main.3d441ee4.chunk.js"
+  },
+  {
+    "revision": "608e613b53c5d73e7b22",
+    "url": "/static/js/runtime-main.078d673d.js"
+  },
+  {
+    "revision": "69766e6b79b3dc95b02dbee7fd936545",
+    "url": "/static/media/alipay.69766e6b.svg"
+  },
+  {
+    "revision": "6de17e90733fce0859f6c791426b4627",
+    "url": "/static/media/wepay.6de17e90.svg"
+  },
+  {
+    "revision": "483cfa51befe2cbd90fd6b1b17c733d1",
+    "url": "/static/media/westore.483cfa51.svg"
+  }
+]);
